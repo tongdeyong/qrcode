b9923986ded52996921e8e3424fbd9a3d94b896d
童德勇 <313718582@qq.com>
2021-03-08 00:51:42 +0800

    finish timer

---
+++ src/utils/index.js
@@ -0,0 +1,89 @@
+/**
+ * 得到长度为10或13的时间戳
+ * @param length
+ * @returns {string}
+ */
+export function timeUnix_now(length, timeZone) {
+  return strTimeToUnixTime(length, null, timeZone)
+}
+
+/**
+ * 本地默认时间
+ * @returns {string}
+ */
+export function time_default_now(unixTime, timeZone) {
+  return unixTimeToStrTime('Y-m-d H:i:s', unixTime, timeZone)
+}
+
+/**
+ * 时间字符串转时间戳
+ * @param strTime   时间字符串 Y-m-d H:i:s,只限这种格式
+ * @param length    时间戳长度
+ * @param timeZone  时区
+ * @returns {string}
+ */
+export function strTimeToUnixTime(length, strTime, timeZone) {
+  if (parseInt(length) !== 13 && parseInt(length) !== 10) {
+    console.log('length必须为10或者13，默认为10')
+    length = 10
+  }
+  let date
+  if (strTime) {
+    date = new Date(strTime)
+  } else {
+    date = new Date()
+  }
+  const nowDate = date.getTime()
+  if (timeZone == null || timeZone === '') {
+    timeZone = 8
+  }
+  const offset_GMT = new Date().getTimezoneOffset()
+  const targetDate = nowDate - offset_GMT * 60 * 1000 - timeZone * 60 * 60 * 1000
+  return targetDate.toString().slice(0, length)
+}
+
+/**
+ * 时间戳转换为时间 Y-m-d H:i:s
+ * @param format
+ * @param unixTime
+ * @param timeZone
+ * @returns {string}
+ */
+export function unixTimeToStrTime(format, unixTime, timeZone) {
+  if (unixTime) {
+    unixTime = parseInt(unixTime.toString().length === 10 ? unixTime * 1000 : unixTime)
+  } else {
+    unixTime = new Date().getTime()
+  }
+  if (timeZone == null || timeZone === '') {
+    timeZone = 8
+  }
+  const offset_GMT = new Date().getTimezoneOffset()
+  unixTime += (offset_GMT * 60 * 1000 + timeZone * 60 * 60 * 1000)
+  const date = new Date(unixTime)
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  const t = {
+    Y: date.getFullYear(),
+    m: padTo2(month),
+    d: padTo2(day),
+    H: padTo2(hours),
+    i: padTo2(minutes),
+    s: padTo2(seconds)
+  }
+  format = format || 'Y-m-d H:i:s'
+  return format.replace(/[\]?[a-zA-Z]/g, function(w) {
+    if (w === '\\') {
+      return '-'
+    } else {
+      return t[w]
+    }
+  })
+}
+
+function padTo2(n) {
+  return n > 9 ? n : '0' + n
+}
--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -9,9 +9,11 @@
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#">选择时区：</a></li>
             <li>
-              <select class="form-control">
-                <option>北京(GMT +8)</option>
-              </select>
+              <label>
+                <select v-model="timeZone" class="form-control" @change="changeTimeZone">
+                  <option v-for="zone in timeZones" :key="zone.value" :value="zone.value">{{ zone.label }}</option>
+                </select>
+              </label>
             </li>
           </ul>
         </div>
@@ -21,21 +23,21 @@
       <h4 class="content-title">&gt;&gt; Unix时间戳定义</h4>
       <form class="form-inline">
         <div class="form-group">
-          <label>现在的当地时间为：</label>
+          <label style="width: 127px">现在的当地时间为:</label>
           <label>
             <input v-model="localTime" type="text" class="form-control" readonly>
           </label>
         </div>
-        <button type="submit" class="btn btn-primary" style="margin-left: 10px" @click="status = status > 0 ? 0:1 ">{{ status>0?'暂停':'开始' }}</button>
+        <button type="submit" class="btn btn-primary" style="margin-left: 25px" @click="status = status > 0 ? 0:1 ">{{ status>0?'暂停':'开始' }}</button>
       </form>
       <form class="form-inline">
         <div class="form-group">
-          <label>现在的Unix时间戳：</label>
+          <label style="width: 127px">现在的Unix时间戳:</label>
           <label>
             <input v-model="timetamp" type="text" class="form-control" readonly>
             秒
           </label>
-          <label style="margin-left: 30px">
+          <label style="margin-left: 4px">
             <input v-model="timetamp_now" type="text" class="form-control" readonly>
             毫秒
           </label>
@@ -45,17 +47,11 @@
       <form class="form-inline">
         <div class="form-group">
           <label>
-            <input type="text" class="form-control" placeholder="时间">
+            <input v-model="timetampInput" type="text" class="form-control" :placeholder="'时间戳eg:' + timetamp">
           </label>
+          <button type="button" class="btn btn-primary" style="margin: 0 25px" @click="UnixToTime">转换</button>
           <label>
-            <select class="form-control" style="margin-left: 5px">
-              <option value="0">秒(s)</option>
-              <option value="1">毫秒(ms)</option>
-            </select>
-          </label>
-          <button type="button" class="btn btn-primary" style="margin: 0 25px">转换</button>
-          <label>
-            <input type="text" class="form-control" placeholder="时间">
+            <input v-model="timetampOutput" type="text" class="form-control" placeholder="时间">
           </label>
         </div>
       </form>
@@ -63,14 +59,14 @@
       <form class="form-inline">
         <div class="form-group">
           <label>
-            <input type="text" class="form-control" placeholder="时间">
+            <input v-model="timeInput" type="text" class="form-control" :placeholder="'时间eg:' + localTime">
           </label>
-          <button type="button" class="btn btn-primary" style="margin: 0 25px">转换</button>
+          <button type="button" class="btn btn-primary" style="margin: 0 25px" @click="timeToUnix">转换</button>
           <label>
-            <input type="text" class="form-control" placeholder="时间">
+            <input v-model="timeOutput" type="text" class="form-control" placeholder="时间时间戳">
           </label>
           <label>
-            <select class="form-control" style="margin-left: 5px">
+            <select v-model="timeType" class="form-control" style="margin-left: 5px" @change="timeToUnix">
               <option value="0">秒(s)</option>
               <option value="1">毫秒(ms)</option>
             </select>
@@ -82,6 +78,8 @@
 </template>
 
 <script>
+import { time_default_now, timeUnix_now } from '../utils'
+import { strTimeToUnixTime, unixTimeToStrTime } from '../utils'
 export default {
   name: 'Timer',
   data() {
@@ -91,23 +89,86 @@ export default {
       timetamp_now: 0,
       localTime: '',
       timetamp: 0,
-      timeType: '0'
+      timeType: '0',
+      timeZones: [
+        { label: '埃尼威托克岛 (GMT-12)', value: -12 },
+        { label: '萨摩亚群岛 (GMT-11)', value: -11 },
+        { label: '夏威夷 (GMT-10)', value: -10 },
+        { label: '阿拉斯加 (GMT-9)', value: -9 },
+        { label: '太平洋时间 (GMT-8)', value: -8 },
+        { label: '山脉时间 (GMT-7)', value: -7 },
+        { label: '中央标准时间 (GMT-6)', value: -6 },
+        { label: '东部时间 (GMT-5)', value: -5 },
+        { label: '大西洋时间 (GMT-4)', value: -4 },
+        { label: 'Brazilia (GMT-3)', value: -3 },
+        { label: '大西洋中部时间(GMT-2)', value: -2 },
+        { label: '亚述尔群岛 (GMT-1)', value: -1 },
+        { label: '格林威治标准（GMT）', value: 0 },
+        { label: '罗马 (GMT +1)', value: 1 },
+        { label: '以色列 (GMT +2)', value: 2 },
+        { label: '莫斯科 (GMT +3)', value: 3 },
+        { label: '巴库 (GMT +4)', value: 4 },
+        { label: 'New Delhi (GMT +5)', value: 5 },
+        { label: 'Dhakar (GMT +6)', value: 6 },
+        { label: '曼谷 (GMT +7)', value: 7 },
+        { label: '北京 (GMT +8)', value: 8 },
+        { label: '东京 (GMT +9)', value: 9 },
+        { label: '悉尼 (GMT +10)', value: 10 },
+        { label: 'Magadan (GMT +11)', value: 11 },
+        { label: '惠灵顿 (GMT +12)', value: 12 }
+      ],
+      timeZone: 8,
+      timetampInput: '',
+      timetampOutput: '',
+      timeInput: '',
+      timeOutput: ''
     }
   },
   created() {
     this.getOutputTimestamp()
   },
   methods: {
-    getTimeTamp() {
-      return new Date().getTime().toString()
-    },
     getOutputTimestamp() {
       this.timeObject = setInterval(() => {
         if (this.status > 0) {
-          this.timetamp_now = this.getTimeTamp()
+          this.timetamp_now = timeUnix_now(13, this.timeZone)
           this.timetamp = this.timetamp_now.slice(0, 10)
+          this.localTime = time_default_now(null, this.timeZone)
         }
       }, 1000)
+    },
+    UnixToTime() {
+      this.timetampOutput = ''
+      if (this.timetampInput) {
+        if (typeof this.timetampInput !== 'number') {
+          alert('请填写数字')
+          return
+        }
+        this.timetampOutput = unixTimeToStrTime('Y-m-d H:i:s', this.timetampInput, this.timeZone)
+      } else {
+        alert('先填写你需要转换的时间戳')
+      }
+    },
+    timeToUnix() {
+      let length = 13
+      if (this.timeType === '0') {
+        length = 10
+      }
+      this.timeOutput = ''
+      if (this.timeInput) {
+        if (!/\d{2}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2})?/.test(this.timeInput)) {
+          alert('时间格式为:' + this.localTime + '或' + this.localTime.slice(0, 10))
+          return
+        }
+        this.timeOutput = strTimeToUnixTime(length, this.timeInput, this.timeZone)
+      } else {
+        alert('先填写你需要转换的时间')
+      }
+    },
+    changeTimeZone() {
+      this.getOutputTimestamp()
+      this.timeToUnix()
+      this.UnixToTime()
     }
   }
 }
