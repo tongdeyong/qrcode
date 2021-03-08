e80b384f6c28a360380335a21f21177baa67ec79
童德勇 <313718582@qq.com>
2021-03-09 00:24:22 +0800

    finish qrcode 50%

--- src/views/Qrcode/Analysis/index.vue
+++ src/views/Qrcode/Analysis/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div class="img-div">
-      图片
+      <div class="img">
+        <img v-if="!!imgSrc" :src="imgSrc" alt="待解析图片">
+      </div>
+      <el-button type="primary" @click="choosePictrue">选择上传图片</el-button>
+      <input id="upload-input" type="file" accept="image/gif, image/jpg, image/png" @change="choosePictrue">
     </div>
     <div class="img-div result">
       输出
@@ -12,7 +16,21 @@
 <script>
 // import jsQR from 'jsqr'
 export default {
-  name: 'Index'
+  name: 'Index',
+  data() {
+    return {
+      imgSrc: '',
+      fileInput: ''
+    }
+  },
+  methods: {
+    choosePictrue() {
+      const fileInput = document.getElementById('upload-input')
+      console.log(fileInput.files[0])
+      fileInput.click()
+      this.imgSrc = window.URL.createObjectURL(fileInput.files[0])
+    }
+  }
 }
 </script>
 
@@ -23,6 +41,18 @@ export default {
   border: #ddd 4px dashed;
   height: 460px;
   display: inline-block;
+  vertical-align: middle;
+}
+img {
+  max-width: 300px;
+  max-height: 300px;
+  margin: auto;
+}
+.img {
+  width: 320px;
+  height: 320px;
+  margin: auto;
+  padding: 10px;
 }
 .result {
   height: 460px;
@@ -33,4 +63,10 @@ export default {
   background: #fcfcfc;
   border: 1px solid #ddd;
 }
+.el-button{
+  margin-top: 20px;
+}
+  #upload-input{
+    display: none;
+  }
 </style>
